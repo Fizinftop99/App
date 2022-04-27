d16c35c3773d747d22534077ebdc1d19cfeae7f4
Fizinftop99 <naumov_nik99@mail.ru>
2022-04-27 12:15:42 +0300

    test

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
-import csv
-
 import pandas as pd
 import numpy as np
-from neo4j import GraphDatabase, Transaction
+
+
+def read_graph_data(file_name: str) -> pd.DataFrame:
+    target_cols = ['Идентификатор операции', 'Артикул', 'Название операции', 'Последователи']
+    df = pd.read_excel(file_name, dtype=str)
+    exist_cols = df.columns.values.tolist()
+    if 'ADCM_П/п' in exist_cols:
+        df.rename(columns={'ADCM_П/п': 'Артикул'}, inplace=True)
+
+    df = df[target_cols]
+    df = df.loc[df['Артикул'].notna()]
+    df.loc[:, 'Идентификатор операции'] = df['Идентификатор операции'].apply(str.strip)
+    df = df[df['Идентификатор операции'].str.startswith('R')]
+    df.set_index('Идентификатор операции', inplace=True)  # Update indeces
+
+    return df
 
 
 def main():
-    q_data_obtain = '''
-                    MATCH (n)-[r]->(m)
-                    RETURN n.name AS name1, n.id AS id1, properties(r).weight AS weight, m.name AS name2, m.id AS id2
-                    '''
-    driver = GraphDatabase.driver("neo4j://20.107.79.39:7687", auth=("neo4j", "Accelerati0n"))
-    with driver.session() as session:
-        result = session.run(q_data_obtain).data()
-        df = pd.DataFrame(result)
-        save_path = 'C:\\Users\\Nikita\\.Neo4jDesktop\\relate-data\\dbmss\\dbms-44d5c24b-bb41-4e4b-bbeb-f18bca851f09\\import\\'
-        df.to_csv(save_path + '2.csv', index=False)
-    driver.close()
-
-    # with open(, 'w') as f:
-    #     writer = csv.writer(f)
-    #     header = ['n_id', 'n_name', 'r_weight', 'm_id', 'm_name']
-    #     for i in result:
-    #         row = [i[]]
-    #         writer.writerow(row)
+    file_name = 'data/solution_schedule.xlsx'
+    df = pd.read_excel(file_name, sheet_name="Состав работ", dtype=str, index_col=0)
+    vendors = df['vendor_code'].tolist()
+    print(vendors)
+    print(df.columns)
 
 
 if __name__ == '__main__':
