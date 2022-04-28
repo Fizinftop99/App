95ff64d89f9a7cca3aed6b3a95ad32d3a4967eb8
Fizinftop99 <naumov_nik99@mail.ru>
2022-04-28 19:27:17 +0300

    +

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ from neo4j import GraphDatabase
 import numpy as np
 import datetime
 import xlsxwriter
+import os
+from openpyxl import load_workbook
+
+src_uri = "neo4j+s://174cd36c.databases.neo4j.io:7687"
+src_user = "neo4j"
+src_password = "w21V4bw-6kTp9hceHMbnlt5L9X1M4upuuq2nD7tD_xU"
+uri = "bolt://localhost:7687"
+user = "neo4j"
+password = "2310"
+
+
+# src_uri = "neo4j://20.107.79.39:7687"
+# src_user = "neo4j"
+# src_password = "Accelerati0n"
 
 
 class App:
@@ -16,36 +30,38 @@ class App:
     def load_data(self, remote_uri, remote_user, remote_pswd):
         q_data_obtain = '''
             MATCH (n)-[r]->(m)
-            RETURN n.name AS name1, n.id AS id1, properties(r).weight AS weight, m.name AS name2, m.id AS id2
+            RETURN n.name AS n_name, n.id AS n_id, properties(r).weight AS weight, m.name AS m_name, m.id AS m_id
             '''
         q_create = '''
             LOAD CSV WITH HEADERS FROM 'file:///2.csv' AS row
-            MERGE (n:Work {id: row.id1, name: row.name1})
-            MERGE (m:Work {id: row.id2, name: row.name2})
+            MERGE (n:Work {id: row.n_id, name: row.n_name})
+            MERGE (m:Work {id: row.m_id, name: row.m_name})
             CREATE (n)-[r:FOLLOWS {weight: row.weight}]->(m);
             '''
         # obtaining data
         src_driver = GraphDatabase.driver(remote_uri, auth=(remote_user, remote_pswd))
+        print('open')
         result = src_driver.session().run(q_data_obtain).data()
         src_driver.close()
         df = pd.DataFrame(result)
-        save_path = 'C:\\Users\\Nikita\\.Neo4jDesktop\\relate-data\\dbmss\\dbms-44d5c24b-bb41-4e4b-bbeb-f18bca851f09\\import\\'
+        save_path = 'C:\\Users\\Nikita\\.Neo4jDesktop\\relate-data\\dbmss\\dbms-44d5c24b-bb41-4e4b-bbeb-f18bca851f09' \
+                    '\\import\\'
         df.to_csv(save_path + '2.csv', index=False)
         with self.driver.session() as session:
             session.run("MATCH (n) DETACH DELETE n")  # Очистка
             session.run(q_create)
 
     def removing_node(self, id: str):
-        income_data_obtain = f'''
-                MATCH (n)-[]->(m)
-                WHERE m.id = $id
-                RETURN n
-                '''
-        outcome_data_obtain = f'''
-                    MATCH (n)-[]->(m)
-                    WHERE n.id = $id
-                    RETURN m
-                    '''
+        income_data_obtain = '''
+            MATCH (n)-[]->(m)
+            WHERE m.id = $id
+            RETURN n
+            '''
+        outcome_data_obtain = '''
+            MATCH (n)-[]->(m)
+            WHERE n.id = $id
+            RETURN m
+            '''
         with self.driver.session() as session:
             incoming = session.run(income_data_obtain, id=id).data()
             outcoming = session.run(outcome_data_obtain, id=id).data()
@@ -136,21 +152,61 @@ class App:
             '''
         self.driver.session().run(q_delete)
 
+    def ordering(self, filename):
+        q_data_obtain = '''
+        MATCH (n)-[]->(m)
+        RETURN n.id AS n_id, m.id AS m_id
+        '''
+        file_name = 'data/solution_schedule.xlsx'
+        schedDF = pd.read_excel(file_name, sheet_name="Состав работ", dtype=str, index_col=0)
+        schedDF = schedDF[['wbs2', 'vendor_code']]  # , 'name']
+        schedDF['precursors'] = ''
+        schedDF['followers'] = ''
+        wbs2_arr = schedDF.wbs2.unique()
+
+        # checking
+        # vendors = schedDF[schedDF.wbs2 == wbs2_arr[0]].vendor_code.to_numpy()
+        # self.load_data(src_uri, src_user, src_password)
+        # self.new_graph(vendors)
+        # self.del_extra_rel()
+
+        for wbs2 in reversed(wbs2_arr):
+            wbs_df = schedDF[schedDF.wbs2 == wbs2]
+            vendors = wbs_df.vendor_code.to_numpy()
+            self.load_data(src_uri, src_user, src_password)
+            self.new_graph(vendors)
+            self.del_extra_rel()
+
+            # Должно быть цикле
+            result = self.driver.session().run(q_data_obtain).data()
+            df = pd.DataFrame(result)
+            for ind, vend in enumerate(vendors):
+                ind2 = wbs_df.index[wbs_df.vendor_code == vend].tolist()[0]
+                # if ind != ind2[0]:
+                #     print(ind, ind2, vend)
+                flwDF = df.loc[df.n_id == vend]
+                if not flwDF.empty:
+                    flws = flwDF.m_id.to_numpy()
+                    wbs_df.at[ind2, 'followers'] = ', '.join(flws)
+
+                predDF = df.loc[df.m_id == vend]
+                if not predDF.empty:
+                    preds = predDF.n_id.to_numpy()
+                    wbs_df.at[ind2, 'precursors'] = ', '.join(preds)
+
+            # book = load_workbook('data/new_sched.xlsx')
+            with pd.ExcelWriter('new_sched.xlsx', engine='openpyxl', mode='a') as writer:
+                # writer.book = book
+                wbs_df.to_excel(writer, sheet_name=wbs2)
+                writer.save()
+
+
 
 def main():
     starttime = datetime.datetime.now()
-    src_uri = "neo4j+s://174cd36c.databases.neo4j.io"
-    src_user = "neo4j"
-    src_password = "w21V4bw-6kTp9hceHMbnlt5L9X1M4upuuq2nD7tD_xU"
-    uri = "bolt://localhost:7687"
-    user = "neo4j"
-    password = "2310"
-    # uri = "neo4j://20.107.79.39:7687"
-    # user = "neo4j"
-    # password = "Accelerati0n"
-
     app = App(uri, user, password)
-    app.load_data(src_uri, src_user, src_password)
+    # app.load_data(src_uri, src_user, src_password)
+    app.ordering('data/solution_schedule.xlsx')
     print('data loaded', datetime.datetime.now() - starttime)
     # app.new_graph(['100203',  # Монтаж планка натягивающая
     #                '171670',  # Монтаж трос стен вi-l дл.5986 серьга вверху
